37e46e758340be4ac5ab831652807772185c5d12
Nil Ordoñez <nilordonez.7@gmail.com>
2018-04-19 15:32:33 +0200

    Initial Commit

---
+++ M08-UF2-PracticaFinal_BMerelo_NOrdonez/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  M08-UF2-PracticaFinal_BMerelo_NOrdonez
+//
+//  Created by Nil Ordoñez Romera on 19/4/18.
+//  Copyright © 2018 Nil Ordoñez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
